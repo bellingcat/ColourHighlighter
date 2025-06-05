8398a0536523352bd45c4ac61e1e4987965e6882
Galen Reich <54807169+GalenReich@users.noreply.github.com>
2025-06-05 18:58:23 +0100

    Enhanced RGB configs

--- configs.js
+++ configs.js
@@ -33,12 +33,12 @@ export const filterConfigs = [
     },
     {
         id: "blue-enhanced",
-        name: "Blue (Enhanced)",
+        name: "Blue+",
         lut: "LUTs/blue-isolated.png",
         enableLUT: true,
         chromaKeys: [
             {
-                ckey_color: [80 / 255, 101 / 255, 102 / 255],
+                ckey_color: [41 / 255, 49 / 255, 36 / 255],
                 ckey_similarity: 10.0 / 255.0,
                 ckey_smoothness: 31.0,
                 ckey_spill: 195.0
@@ -49,6 +49,50 @@ export const filterConfigs = [
                 gamma: -0.06,
                 contrast: 0.23,
                 saturation: 1.39
+            }
+        ]
+    },
+    {
+        id: "red-enhanced",
+        name: "Red+",
+        lut: "LUTs/red-isolated.png",
+        enableLUT: true,
+        chromaKeys: [
+            {
+                ckey_color: [91 / 255, 0 / 115, 187 / 255],
+                ckey_similarity: 1.0 / 255.0,
+                ckey_smoothness: 1.0,
+                ckey_spill: 347.0
+            }
+        ],
+        colorCorrections: [
+            {
+                gamma: 0.03,
+                contrast: 0.13,
+                brightness: 0.0493,
+                saturation: 3.3
+            },
+
+        ]
+    },
+    {
+        id: "green-enhanced",
+        name: "Green+",
+        lut: "LUTs/green-isolated.png",
+        enableLUT: true,
+        chromaKeys: [
+            {
+                ckey_color: [38 / 255, 47 / 255, 40 / 255],
+                ckey_similarity: 1.0 / 255.0,
+                ckey_smoothness: 1.0,
+                ckey_spill: 30.0
+            }
+        ],
+        colorCorrections: [
+            {
+                gamma: -0.15,
+                contrast: 0.25,
+                saturation: 0.34
             }
         ]
     }
